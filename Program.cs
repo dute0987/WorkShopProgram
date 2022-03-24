981a9e96027b0327bda9e5d97a34e1167217c793
dute0987 <dute0987@gmail.com>
2022-03-24 10:54:41 +0530

    Vishal Dute - Main file for access the Power of Two Program

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace PowerOfTwoReview
+{
+    class Program
+    {
+        public static void Main(string[]args)
+        {
+            Console.WriteLine("Welcome to the Power of Two Program");
+            Console.WriteLine("please Enter Value for upto which want the Power Value ");
+           
+            int n = Convert.ToInt32(Console.ReadLine());
+            int x = 1;
+            const int y = 2;
+            for(int i = 0; i < n; i++)
+            {
+                x = x * y;
+            }
+            Console.WriteLine("The Power of Two is : " + x);
+        }
+    }
+}
